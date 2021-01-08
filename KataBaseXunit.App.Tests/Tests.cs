89550a87ac609502eba056103390da92b7b2207c
vincent.lee@auden.co.uk <vincent.lee@auden.co.uk>
2021-01-08 13:34:17 +0000

    initial commit

---
+++ KataBaseXunit.App.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace KataBaseXunit.App.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void FooShouldGiveBar()
+        {
+            Assert.Equal("bar", new Class1().Foo());
+        }
+    }
+}
---
+++ KataBaseXunit.App/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KataBaseXunit.App
+{
+    public class Class1
+    {
+        public string Foo()
+        {
+            return "bar";
+        }
+    }
+}
